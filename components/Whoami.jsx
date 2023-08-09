da3e731194c2d1ba75895855b1239ac2fad165b4
Quertyy <qwertyense@protonmail.com>
2023-08-09 13:19:44 +0200

    feat: add line + cursor at whoami bottom

--- components/Whoami.jsx
+++ components/Whoami.jsx
@@ -10,6 +10,13 @@ const Whoami = () => {
                 <p className="text-md">sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>
             </div>
             <h1 className="text-3xl font-bold py-4">{'}'}</h1>
+            <div className="relative flex py-5 items-center">
+                <div className="flex-grow border-t border-green"></div>
+                <div className="inline-block pl-1 relative">
+                    <span className="cursor-blink bg-green h-[22px] w-[10px] block ml-[0.25rem] absolute bottom-[-1px]"></span>
+                </div>
+            </div>
+
         </div>
       );
       
