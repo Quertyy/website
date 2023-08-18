15f8c4231d2b91ff4ee3fa66bb56b63eac3409c4
Quertyy <qwertyense@protonmail.com>
2023-08-18 17:38:24 +0200

    feat: add career + projects pages

---
+++ app/projects/page.jsx
@@ -0,0 +1,17 @@
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+
+function Projects() {
+    return (
+        <>
+        <div className="bg-gray">
+          <Header />
+          <main className="py-10">
+          </main>
+        </div>
+        <Footer />
+    </>
+    );
+}
+
+export default Projects;
